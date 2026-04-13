1904a23d397ec200cfc47be9bc88eab5135bd11d
Thiago Shigueto Hossaka <126188799+tshigueto@users.noreply.github.com>
2026-04-13 14:47:22 -0600

    Correção de bug
    Removed commented-out code and updated resource location format.

--- caminhaoApp/src/main/java/com/caminhaoApp/caminhaoApp/config/WebConfig.java
+++ caminhaoApp/src/main/java/com/caminhaoApp/caminhaoApp/config/WebConfig.java
@@ -8,38 +8,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    // Pega o caminho C:/uploads/caminhaoapp/ do seu application.properties
     @Value("${upload.path}")
     private String uploadPath;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Cria a rota virtual "/uploads/" que aponta para a pasta física
         registry.addResourceHandler("/uploads/**")
                 .addResourceLocations("file:" + uploadPath);
     }
 }
 
-
-
-/* package com.caminhaoApp.caminhaoApp.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-
-    // Pega o caminho C:/uploads/caminhaoapp/ do seu application.properties
-    @Value("${upload.path}")
-    private String uploadPath;
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // Cria a rota virtual "/uploads/" que aponta para a pasta física
-        registry.addResourceHandler("/uploads/**")
-                .addResourceLocations("file:///" + uploadPath);
-    }
-}*/
